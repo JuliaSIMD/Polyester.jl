9f57dc348ac83c8117ed55445712ef211bc9826a
Chris Elrod <elrodc@gmail.com>
2021-08-27 18:08:52 -0400

    Add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -95,6 +95,10 @@ function issue25!(dest, x, y)
 end
 
 @testset "Range Map" begin
+    threads, torelease = Polyester.request_threads(nothing, Threads.nthreads()-1)
+    @test threads isa NTuple{Polyester.worker_mask_count(),Polyester.UnsignedIteratorEarlyStop{Polyester.worker_type()}}
+    @test sum(map(length, threads)) == Threads.nthreads()-1
+    Polyester.free_threads!(torelease)
 
     x = rand(1024); y = rand(length(x)); z = similar(x);
     foo(x,y) = exp(-0.5abs2(x-y))
